1b346509fb2b85167f641d1333b25bc16bafd5e6
yousef Kablawy <yousefkablawy25@gmail.com>
2022-03-23 17:46:33 +0200

    frontend added successfully

--- job/models.py
+++ job/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from pydoc import describe
 from django.db import models
 from django.forms import CharField
@@ -15,6 +16,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     experience = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='jobs/')
 
     def __str__(self):
         return self.title
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,8 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
